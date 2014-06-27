c2cef15604b4e37680d64b3173d83c0898439412
Chad <simpleGrowth@users.noreply.github.com>
2014-06-27 19:24:37 -0400

    Route fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
-
-
-
   resources :restaurants, :only => [:index, :show] do
-
-      resources :reservations, :only => [:show, :create, :edit, :destroy]
-      resources :reviews, :only => [:create, :destroy, :show]
-
+    resources :reservations, :only => [:index, :show, :create, :edit, :destroy]
+    resources :reviews, :only => [:create, :destroy, :show]
   end
 
   resources :sessions, :only => [:new, :create, :destroy]
