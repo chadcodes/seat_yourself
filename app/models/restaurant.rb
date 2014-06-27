885f9d2babaab48d310fb07c9c6cbe76fee1d0f5
Sandy Ludosky <sandyludosky@Sandys-MacBook-Pro.local>
2014-06-26 23:42:41 -0400

    Adding scoping to display 5 featured restaurants - criteria to define (popularity or availability of seats)

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -6,7 +6,8 @@ class Restaurant < ActiveRecord::Base
   find(:all, :conditions => ['title LIKE ? OR description LIKE ?', search_condition, search_condition])
 end
 
-  scope :most_recent_five, -> { limit(5) }
+ 
+  scope :most_recent_five, -> { all.limit(5) }
   scope :created_before, ->(time) { where("created_at < ?", time) }
 
 
