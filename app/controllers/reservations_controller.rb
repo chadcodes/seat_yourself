07ff38e4af33a8077f7df96fc1f57d151a60536c
Chad <simpleGrowth@users.noreply.github.com>
2014-06-27 16:30:17 -0400

    Reservation index, show and edit plus route changes

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,14 +1,42 @@
 class ReservationsController < ApplicationController
-  def index
-  	@reservations = Reservation.all
-  end
+  before_filter :ensure_logged_in
+
+  # def index
+  # 	# @reservations = Reservation.all
+  #   @restaurant = Restaurant.find(params[:id])
+  # end
 
   def show
   	@reservation = Reservation.find(params[:id])
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    if @reservation.user_id != current_user.id
+      redirect_to restaurants_path, alert: 'This is not your reservation'
+    end
   end
 
-
-  def reservation_params
-  	params.require(:reservation).permit(:date, :guests, :time_slot,:user_id,:restaurant_id)
+  def edit
+    # not sure we need an edit page here.
+    @reservation = Reservation.find(params[:id])
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
+
+  def destroy
+    @reservation = Reservation.find(params[:id])
+    if @reservation.destroy
+      redirect_to restaurants_path, notice: 'Your reservation was canceled.'
+    else
+      flash.now[:alert] = "Something bad happened.  Your reservation still exists."
+      render "show"
+    end
+  end
+
+  private
+    def reservation_params
+    	params.require(:reservation).permit(
+        :date,
+        :guests,
+        :time_slot,
+        :user_id,
+        :restaurant_id)
+    end
 end
