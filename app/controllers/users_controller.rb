61b3b3fea3a43b88c25dabcc76b83d4ea69d5073
Sandy Ludosky <sandyludosky@Sandys-MacBook-Pro.local>
2014-06-27 16:24:32 -0400

    Edited user controller/view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,7 @@
 class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
-    @user_reservations = @user.reservations
-    @all_user_reservations = @user.restaurants
-  
+
 
   end
 
