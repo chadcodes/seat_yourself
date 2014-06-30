86f141ff570dba53858d6f4a7ae833b79432663d
Chad <simpleGrowth@users.noreply.github.com>
2014-06-30 17:30:38 -0400

    Update restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,7 +2,12 @@ class Restaurant < ActiveRecord::Base
   has_many :reservations
   has_many :reviews
   has_many :users, :through => :reservations
-  has_one :user
+  # this is for restaurant owner, will add later
+  # has_one :user
+
+  # used for geocoding
+  geocoded_by :address
+  after_validation :geocode
 
   def self.search(query)
     # where(:title, query) -> This would return an exact match of the query
