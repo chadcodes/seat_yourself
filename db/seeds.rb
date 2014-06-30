5b28a6381289ce101907eb0c6778610d61d57a4a
Chad <simpleGrowth@users.noreply.github.com>
2014-06-30 19:21:20 -0400

    Update seed and Restaurant model

--- db/seeds.rb
+++ db/seeds.rb
@@ -46,21 +46,21 @@
 # end
 
 # create some restaurants
-restaurants = []
-i = 0
-8.times do
-  r_name = ["Ponderosa", "Pizza Delight", "Boston Pizza", "Captain Johns", "St. Huberts", "Swiss Chalet", "Xola", "Beach Tree"]
-  menu = "stuff"
-  seats = 50
-  street_address = "54 Main St."
-  postal_code = "M1M 1M1"
-   photo_url = "http://lorempixel.com/450/450/food/" + rand(1..9).to_s + "/"
-  price_range = rand(1..4)
-  cuisine = ["Canadian", "Pizza", "Italian", "Seafood", "Chicken", "Rotisserie", "Mexican", "British"]
-  new_restaurant = [r_name[i], menu, seats, street_address, postal_code, photo_url, price_range, cuisine[i]]
-  restaurants.push(new_restaurant)
-  i += 1
-end
+# restaurants = []
+# i = 0
+# 8.times do
+#   r_name = ["Ponderosa", "Pizza Delight", "Boston Pizza", "Captain Johns", "St. Huberts", "Swiss Chalet", "Xola", "Beach Tree"]
+#   menu = "stuff"
+#   seats = 50
+#   street_address = "54 Main St."
+#   postal_code = "M1M 1M1"
+#    photo_url = "http://lorempixel.com/450/450/food/" + rand(1..9).to_s + "/"
+#   price_range = rand(1..4)
+#   cuisine = ["Canadian", "Pizza", "Italian", "Seafood", "Chicken", "Rotisserie", "Mexican", "British"]
+#   new_restaurant = [r_name[i], menu, seats, street_address, postal_code, photo_url, price_range, cuisine[i]]
+#   restaurants.push(new_restaurant)
+#   i += 1
+# end
 
 
 # restaurants.each do |name, menu, seats, street_address, postal_code, photo_url, price_range, cuisine|
@@ -77,20 +77,20 @@ end
 
 
 # create some random reservations
-# reservations = []
-# 300.times do
-#   restaurant_id = rand(1..8)
-#   user_id = rand(1..10)
-#   guests = rand(1..4)
-#   date = DateTime.now.strftime("%Y-%m-%d "+rand(11..20).to_s)
-#   new_reservation = [restaurant_id, user_id, guests, date]
-#   reservations.push(new_reservation)
-# end
+reservations = []
+300.times do
+  restaurant_id = rand(9..30)
+  user_id = rand(1..10)
+  guests = rand(1..4)
+  date = DateTime.now.strftime("%Y-%m-%d "+rand(11..20).to_s)
+  new_reservation = [restaurant_id, user_id, guests, date]
+  reservations.push(new_reservation)
+end
 
-# reservations.each do |restaurant_id, user_id, guests, date|
-#   Reservation.create(
-#     :restaurant_id => restaurant_id,
-#     :user_id => user_id,
-#     :guests => guests,
-#     :date => date)
-# end
+reservations.each do |restaurant_id, user_id, guests, date|
+  Reservation.create(
+    :restaurant_id => restaurant_id,
+    :user_id => user_id,
+    :guests => guests,
+    :date => date)
+end
